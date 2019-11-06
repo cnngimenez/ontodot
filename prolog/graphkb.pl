f15a2cc6979d86e30a4cc8cbbf070e28c50d3bf8
cnngimenez <none@none>
2019-11-06 13:15:20 -0300

    Using an output parameter instead of the stdout.

--- prolog/graphkb.pl
+++ prolog/graphkb.pl
@@ -1,4 +1,4 @@
-/*   graphkb.pl
+/*   ontodot.pl
      Author: Giménez, Christian.
 
      Copyright (C) 2019 Giménez, Christian
@@ -20,12 +20,12 @@
 */
 
 
-:- module(graphkb, [
+:- module(ontodot, [
               load_ttl/1,
-              draw_all/0,
-              draw_graph/1,
-              draw_prefix/1,
-              draw_hierarchy/0,
+              draw_all/1,
+              draw_graph/2,
+              draw_prefix/2,
+              draw_hierarchy/1,
               list_nodes/1,
               dot_all/1,
               dot_prefix/2,
@@ -178,89 +178,98 @@ abbrev_name(Name, Abbrev) :-
 abbrev_name(Name, Name).
 
 /**
- draw_node(+Node: term) is det
+ draw_node(+Node: term, -Str: string) is det
 
-Print to stdout a dot representation of the Node.
+Generate a dot representation of the Node.
 
-Print a dot graph node of the given RDF graph Node. Change the representation 
-according to the type of the node.
+Generate a dot graph node of the given RDF graph Node. Change the 
+representation according to the type of the node.
 
 If it is a datatype (is not an IRI), use a box node.
 If it is an IRI, use an ellipse node.
 
 @param Node A prefix:suffix form or IRI.
+@param Str The resulting dot string.
 */
 
-draw_node(Node) :-
+draw_node(Node, Str) :-
     \+ rdf_is_iri(Node), !,
-    format('"~w" [shape=box, color=blue];', [Node]).    
-draw_node(Node) :-
+    format(string(Str), '"~w" [shape=box, color=blue];', [Node]).    
+draw_node(Node, Str) :-
     abbrev_name(Node, Name),
     (Name = owl:_Suffix ; Name = rdf:_Suffix ; Name = rdfs:_Suffix), !,
-    format('"~w" [color=red, shape=ellipse];', [Name]).
-draw_node(Node) :-
+    format(string(Str), '"~w" [color=red, shape=ellipse];', [Name]).
+draw_node(Node, Str) :-
     rdf(Node, rdf:type, owl:'NamedIndividual'),!,
     abbrev_name(Node, Name),
-    format('"~w" [color=blue];', [Name]).
-draw_node(Node) :-
+    format(string(Str), '"~w" [color=blue];', [Name]).
+draw_node(Node, Str) :-
     rdf(Node, rdf:type, owl:'Class'),!,
     abbrev_name(Node, Name),
-    format('"~w" [shape=ellipse];', [Name]).
-draw_node(Node) :-
+    format(string(Str), '"~w" [shape=ellipse];', [Name]).
+draw_node(Node, Str) :-
     %% rdf_is_object(Node),!,
     abbrev_name(Node, Name),
-    format('"~w" [shape=box, color=blue];', [Name]).
+    format(string(Str), '"~w" [shape=box, color=blue];', [Name]).
 
 /**
- draw_edge(+Triple: term) is det
+ draw_edge(+Triple: term, -Str: string) is det
 
-Print to stdout a dot representation of the given edge.
+Generate a dot representation of the given edge.
 
-Use the Triple to print a dot representation of the edge of the graph. The 
-nodes are printed with draw_node/1. 
+Use the Triple to create a dot representation of the edge of the graph. The 
+node dot string is generated with draw_node/1. 
 
 @param Tripe A `(Subject, Pred, Object)` triple representation.
+@param Str The resulting dot string.
 */
-draw_edge((A, B, C)) :-
+draw_edge((A, B, C), Str) :-
     abbrev_name(B, rdfs:subClassOf),
     abbrev_name(A, A2),
     abbrev_name(C, C2),
-    format('edge [label="~w", style=solid, arrowhead=none] "~w" -> "~w";\n',
-           [rdfs:subclassOf, A2, C2]).
-draw_edge((A, B, C)) :-
+    format(
+        string(Str),
+        'edge [label="~w", style=solid, arrowhead=none] "~w" -> "~w";\n',
+        [rdfs:subclassOf, A2, C2]).
+draw_edge((A, B, C), Str) :-
     abbrev_name(A, A2),
     abbrev_name(B, B2),
     abbrev_name(C, C2),
-    format('edge [label="~w", style=dashed, arrowhead=normal] "~w" -> "~w";\n',
-           [B2, A2, C2]).
+    format(
+        string(Str),
+        'edge [label="~w", style=dashed, arrowhead=normal] "~w" -> "~w";\n',
+        [B2, A2, C2]).
 
 /**
- draw_edges_noprops(+Assocs: list)
+ draw_edges_noprops(+Assocs: list, -Str: string)
 
 Draw all edges that has no properties as object.
 
 @param Assocs A list of (S,P,O) triples. 
 @see draw_edge/1
 */
-draw_edges_noprops([]) :- !.
-draw_edges_noprops([(_A, _B, C)|Rest]) :-
+draw_edges_noprops([], "") :- !.
+draw_edges_noprops([(_A, _B, C)|Rest], Str) :-
+    % Ignore if it is an IRI, is probably a property.
     \+ rdf_is_iri(C), !, %% red cut
-    draw_edges_noprops(Rest).
-draw_edges_noprops([Edge|Rest]) :-
-    draw_edge(Edge),
-    draw_edges_noprops(Rest).
+    draw_edges_noprops(Rest, Str).
+draw_edges_noprops([Edge|Rest], Str) :-
+    draw_edge(Edge, Str),
+    draw_edges_noprops(Rest, RestStr),
+    string_concat(Str, RestStr, Str).
 
 /**
- draw_edges(+Triples: list) is det
+ draw_edges(+Triples: list, -Str: string) is det
 
 Draw all the edges on the list using draw_edge/1.
 
 @param Triples A list of `(S, P, O)` terms.
 */
-draw_edges([]) :- !.
-draw_edges([Assoc|Rest]) :-
-    draw_edge(Assoc),
-    draw_edges(Rest).
+draw_edges([], "") :- !.
+draw_edges([Assoc|Rest], Str) :-
+    draw_edge(Assoc, EdgeStr),
+    draw_edges(Rest, RestStr),
+    string_concat(EdgeStr, RestStr, Str).
 
 
 
@@ -273,29 +282,37 @@ Use the subject and object nodes only.
 
 @param Triples A list of `(S, P, O)` terms.
 */
-draw_nodes([]) :- !.
-draw_nodes([(S, _P, O)|Rest]) :-
-    draw_node(S), nl,
-    draw_node(O), nl,
-    draw_nodes(Rest).
+draw_nodes([], "") :- !.
+draw_nodes([(S, _P, O)|Rest], Str) :-
+    draw_node(S, SubjectStr), 
+    draw_node(O, ObjectStr),
+    
+    draw_nodes(Rest, NodesStr),
+
+    format(string(Str), '~s~n~s~n~s',
+           [SubjectStr, ObjectStr, NodesStr]).
 
 
 /**
- draw_graph(Node) is det
+ draw_graph(+Node: term, -Str: string) is det
 
-Print a dot syntax with a graph with the given Node as a center concept.
+Generate a dot syntax with a graph with the given Node as a center concept.
 
-Print all associations related to the give Node (all subjects and objects 
+Generate all associations related to the give Node (all subjects and objects 
 related to it).
 
 @param Node A prefix:suffix term or an IRI.
+@param Str The resulting dot string.
 */
-draw_graph(Node) :-
+draw_graph(Node, Str) :-
     get_associated(Node, Assocs),
-    write('digraph {'), nl,
-    draw_nodes(Assocs),nl,nl,
-    draw_edges(Assocs),
-    write('}'), nl.
+    
+    draw_nodes(Assocs, NodesStr),
+    draw_edges(Assocs, EdgesStr),
+
+    format(string(Str),
+           'digraph {~n~s~n~n~s}~n',
+           [NodesStr,EdgesStr]).
     
 
 /**
@@ -361,25 +378,29 @@ get_all_isa([Node|NRest], Lst) :-
     
 
 /**
- draw_all.
+ draw_all(-Str: string).
 
-Write to stdout the dot string that represent the default RDF graph.
+Generate the dot string that represent the default RDF graph.
 
-Write to stdout a dot representation of the default RDF graph. To improve readability, property objects are not represented associated with the other subjects. See draw_edges_noprops/1 for more information.
+Generate a dot representation of the default RDF graph. To improve readability, property objects are not represented associated with the other subjects. See draw_edges_noprops/1 for more information.
 
-tell/1 and told/0 can be used to store the string into a file. For example:
-
+This predicate can be used with load_ttl/1. For instance:
 ```
-?- load_ttl('my_kb.ttl'), tell('myfile.dot'), draw_all, told.
+?- load_ttl('my_kb.ttl'), draw_all(Str).
 ```
+
+@param Str The resulting dot string.
 */
-draw_all :-
-    write('digraph {'), nl,    
+draw_all(Str) :-
     list_nodes(Nodes),
     get_all_assocs(Nodes, Assocs),
-    draw_nodes(Assocs), nl, nl,
-    draw_edges_noprops(Assocs),
-    write('}'), nl.
+
+    draw_nodes(Assocs, NodesStr), 
+    draw_edges_noprops(Assocs, EdgesStr),
+
+    format(string(Str),
+           'digraph {~n~s~n~n~s}~n',
+           [NodesStr, EdgesStr]).
 
 
 /**
@@ -396,33 +417,44 @@ nodes_with_prefix(Prefix, Nodes) :-
     findall(Prefix:Name, member(Prefix:Name, AllNodes), Nodes).
 
 /**
- draw_prefix(+Prefix: term)
+ draw_prefix(+Prefix: term, -Str: string)
 
-Write in stdout the dot text with the relations of all the nodes with a certain
+Generate the dot text with the relations of all the nodes with a certain
 prefix.
 
 @param Prefix A term.
+@param Str The resulting dot string.
 */
-draw_prefix(Prefix) :-
-    write('digraph {'), nl,
+draw_prefix(Prefix, Str) :-
     nodes_with_prefix(Prefix, Nodes),
     get_all_assocs(Nodes, Assocs),
-    draw_nodes(Assocs), nl, nl,
-    draw_edges(Assocs),
-    write('}'), nl.
+
+    draw_nodes(Assocs, NodesStr),
+    draw_edges(Assocs, EdgesStr),
+
+    format(
+        string(Str),
+        'digraph {~n~s~n~n~s}~n',
+        [NodesStr, EdgesStr]).
 
 /**
- draw_hierarchy.
+ draw_hierarchy(-Str: string).
 
-Write into stdout the dot text with the hierarchy is-a relation of all nodes.
+Generate a dot text with the hierarchy is-a relation of all nodes.
+
+@param Str The resulting dot string.
 */
-draw_hierarchy :-
-    write('digraph {'), nl,
+draw_hierarchy(Str) :-
     list_nodes(AllNodes),
     get_all_isa(AllNodes, Assocs),
-    draw_nodes(Assocs), nl, nl,
-    draw_edges(Assocs),
-    write('}'), nl.
+    
+    draw_nodes(Assocs, NodesStr),
+    draw_edges(Assocs, EdgesStr),
+
+    format(
+        string(Str),
+        'digraph {~n~s~n~n~s}~n',
+        [NodesStr, EdgesStr]).
 
 /**
  prepare_cmd(+File: term, -Stream: term)
@@ -435,8 +467,7 @@ Start the dot command for receiving the dot file from stdin.
 */
 prepare_cmd(File, Stream) :-
     dot_command(File, CMD),
-    open(pipe(CMD), write, Stream),
-    set_output(Stream).
+    open(pipe(CMD), write, Stream).    
 
 /**
  dot_command(+File: term, -CMD: term)
@@ -459,7 +490,8 @@ Create a PNG file with all the relationships of a given node.
 */
 dot_graph(Node, PNGfile) :-
     prepare_cmd(PNGfile, Stream),
-    draw_graph(Node),
+    draw_graph(Node, Str),
+    write(Stream, Str),
     close(Stream).
 
 /**
@@ -471,7 +503,8 @@ Create a PNG file with all the relationships of all the nodes.
 */
 dot_all(PNGfile) :-
     prepare_cmd(PNGfile, Stream),
-    draw_all,
+    draw_all(Str),
+    write(Stream, Str),
     close(Stream).
 
 /**
@@ -485,7 +518,8 @@ relations.
 */
 dot_prefix(Prefix, PNGfile) :-
     prepare_cmd(PNGfile, Stream),
-    draw_prefix(Prefix),
+    draw_prefix(Prefix, Str),
+    write(Stream, Str),
     close(Stream).
 
 /**
@@ -497,5 +531,6 @@ Create a PNG file with all the nodes and their is-a relationships.
 */
 dot_hierarchy(PNGfile) :-
     prepare_cmd(PNGfile, Stream),
-    draw_hierarchy,
+    draw_hierarchy(Str),
+    write(Stream, Str),
     close(Stream).
